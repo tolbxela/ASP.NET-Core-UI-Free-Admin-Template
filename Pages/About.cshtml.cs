8ba22fe3848930fde037e837f5ceb7b5e8dac696
Tolbxela <email@tolbxela.com>
2018-11-26 12:51:53 +0100

    Add project files (ASP.Net Core Template with Razor Pages).

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreUI_Free_Bootstrap_Admin.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+    }
+}
