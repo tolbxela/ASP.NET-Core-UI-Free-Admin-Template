281856fd0d5388a6972f4eb3ac56542cabf949d1
Tolbxela <email@tolbxela.com>
2019-08-08 17:28:55 +0200

    CoreUI Free Bootstrap Admin Template 2.1.8

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CoreUI_Free_Bootstrap_Admin.Pages
+namespace BOP.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return new RedirectResult("/Startseite");
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# ASP.NET Core - CoreUI Free Bootstrap Admin Template 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Made from [CoreUI Free Bootstrap Admin Template](https://github.com/coreui/coreui-free-bootstrap-admin-template)
